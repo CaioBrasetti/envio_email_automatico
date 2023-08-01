274af4d85fb1fc99553c04e6f2a914e7b9c12834
Caio Brasetti <caiobrasetti@hotmail.com>
2023-08-01 17:55:46 -0300

    Adicionando devise a aplicação

--- Gemfile
+++ Gemfile
@@ -3,6 +3,8 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby "3.1.2"
 
+#devise
+gem "devise"
 
 #bootstrap
 gem 'bootstrap', '~> 4.1.3'
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'mes/index'
   get 'mes/mes'
+   
   resources :nivers
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-   root "nivers#index"
+
+  root "nivers#index"
    
 end
